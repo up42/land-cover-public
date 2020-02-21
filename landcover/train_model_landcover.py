f1aded740b034be9197cb407f6cbb60ca20c71f4
Rodrigo Almeida <rodrigo.almeida@up42.com>
2020-02-21 18:19:07 +0100

    Refactor train_model into classes

--- landcover/train_model_landcover.py
+++ landcover/train_model_landcover.py
@@ -166,168 +166,204 @@ def do_args(arg_list, name):
     return parser.parse_args(arg_list)
 
 
+class Train:
+    def __init__(
+        self,
+        output,
+        name,
+        data_dir,
+        training_states,
+        validation_states,
+        superres_states,
+        input_shape=(240, 240, 4),
+        classes=5,
+        verbose=2,
+    ):
+        self.verbose = verbose
+        self.output = output
+        self.name = name
+
+        self.data_dir = data_dir
+        self.training_states = training_states
+        self.validation_states = validation_states
+        self.superres_states = superres_states
+
+        self.input_shape = input_shape
+        self.classes = classes
+
+        self.log_dir = os.path.join(output, name)
+
+        if not os.path.exists(self.log_dir):
+            os.mkdir(self.log_dir)
+
+        # TODO: Fix writing of arguments
+        f = open(os.path.join(self.log_dir, "args.txt"), "w")
+        #for k, v in args.__dict__.items():
+        #    f.write("%s,%s\n" % (str(k), str(v)))
+        f.close()
+
+        print("Starting %s at %s" % (self.name, str(datetime.datetime.now())))
+        self.start_time = float(time.time())
+
+    def load_data(
+        self,
+        batch_size,
+        training_steps_per_epoch,
+        validation_steps_per_epoch,
+        color,
+        do_superres    ):
+        training_patches = []
+        for state in self.training_states:
+            print("Adding training patches from %s" % (state))
+            fn = os.path.join(self.data_dir, "%s_extended-train_patches.csv" % (state))
+            df = pd.read_csv(fn)
+            for fn in df["patch_fn"].values:
+                training_patches.append((os.path.join(self.data_dir, fn), state))
+
+        validation_patches = []
+        for state in self.validation_states:
+            print("Adding validation patches from %s" % (state))
+            fn = os.path.join(self.data_dir, "%s_extended-val_patches.csv" % (state))
+            df = pd.read_csv(fn)
+            for fn in df["patch_fn"].values:
+                validation_patches.append((os.path.join(self.data_dir, fn), state))
+
+        print(
+            "Loaded %d training patches and %d validation patches"
+            % (len(training_patches), len(validation_patches))
+        )
+
+        if do_superres:
+            print("Using %d states in superres loss:" % (len(self.superres_states)))
+            print(self.superres_states)
+
+        training_generator = datagen.DataGenerator(
+            training_patches,
+            batch_size,
+            training_steps_per_epoch,
+            self.input_shape[0],
+            self.input_shape[1],
+            self.input_shape[2],
+            do_color_aug=color,
+            do_superres=do_superres,
+            superres_only_states=self.superres_states,
+        )
+        validation_generator = datagen.DataGenerator(
+            validation_patches,
+            batch_size,
+            validation_steps_per_epoch,
+            self.input_shape[0],
+            self.input_shape[1],
+            self.input_shape[2],
+            do_color_aug=color,
+            do_superres=do_superres,
+            superres_only_states=[],
+        )
+        return training_generator, validation_generator
+
+    def get_model(self, model_type, learning_rate, loss):
+        # Build the model
+        optimizer = RMSprop(learning_rate)
+        if model_type == "unet":
+            model = models.unet(self.input_shape, self.classes, optimizer, loss)
+        elif model_type == "unet_large":
+            model = models.unet_large(self.input_shape, self.classes, optimizer, loss)
+        elif model_type == "fcdensenet":
+            model = models.fcdensenet(self.input_shape, self.classes, optimizer, loss)
+        elif model_type == "fcn_small":
+            model = models.fcn_small(self.input_shape, self.classes, optimizer, loss)
+        model.summary()
+        return model
+
+    def save_model(self, model):
+        model.save(os.path.join(self.log_dir, "final_model.h5"))
+
+        model_json = model.to_json()
+        with open(os.path.join(self.log_dir, "final_model.json"), "w") as json_file:
+            json_file.write(model_json)
+        model.save_weights(os.path.join(self.log_dir, "final_model_weights.h5"))
+
+        print("Finished in %0.4f seconds" % (time.time() - self.start_time))
+
+    def run_experiment(
+        self,
+        epochs,
+        model_type,
+        batch_size,
+        learning_rate,
+        loss,
+        color=False,
+        training_steps_per_epoch=1,
+        validation_steps_per_epoch=1,
+        max_queue_size=256,
+        workers=4,
+    ):
+        do_superres = loss == "superres"
+        #if training_steps_per_epoch * batch_size > len(training_patches):
+        #    training_steps_per_epoch = 300
+        #    validation_steps_per_epoch = 39
+
+        print(
+            "Number of training/validation steps per epoch: %d/%d"
+            % (training_steps_per_epoch, validation_steps_per_epoch)
+        )
+
+        model = self.get_model(model_type, learning_rate, loss)
+
+        validation_callback = utils.LandcoverResults(log_dir=self.log_dir, verbose=self.verbose)
+        learning_rate_callback = LearningRateScheduler(
+            utils.schedule_stepped, verbose=self.verbose
+        )
+
+        model_checkpoint_callback = ModelCheckpoint(
+            os.path.join(self.log_dir, "model_{epoch:02d}.h5"),
+            verbose=self.verbose,
+            save_best_only=False,
+            save_weights_only=False,
+            period=20,
+        )
+
+        training_generator, validation_generator = self.load_data(batch_size,training_steps_per_epoch,validation_steps_per_epoch, color, do_superres)
+
+        model.fit_generator(
+            training_generator,
+            steps_per_epoch=training_steps_per_epoch,
+            epochs=epochs,
+            verbose=self.verbose,
+            validation_data=validation_generator,
+            validation_steps=validation_steps_per_epoch,
+            max_queue_size=max_queue_size,
+            workers=workers,
+            use_multiprocessing=True,
+            callbacks=[
+                validation_callback,
+                # learning_rate_callback,
+                model_checkpoint_callback,
+            ],
+            initial_epoch=0,
+        )
+
+        self.save_model(model)
+
+
 def main():
     prog_name = sys.argv[0]
     args = do_args(sys.argv[1:], prog_name)
-
-    verbose = args.verbose
-    output = args.output
-    name = args.name
-
-    data_dir = args.data_dir
-    training_states = args.training_states
-    validation_states = args.validation_states
-    superres_states = args.superres_states
-
-    num_epochs = args.epochs
-    model_type = args.model_type
-    batch_size = args.batch_size
-    learning_rate = args.learning_rate
-    loss = args.loss
-    do_color_aug = args.color
-    do_superres = loss == "superres"
-
-    log_dir = os.path.join(output, name)
-
-    if not os.path.exists(log_dir):
-        os.mkdir(log_dir)
-
-    f = open(os.path.join(log_dir, "args.txt"), "w")
-    for k, v in args.__dict__.items():
-        f.write("%s,%s\n" % (str(k), str(v)))
-    f.close()
-
-    print("Starting %s at %s" % (prog_name, str(datetime.datetime.now())))
-    start_time = float(time.time())
-
-    # ------------------------------
-    # Step 1, load data
-    # ------------------------------
-
-    training_patches = []
-    for state in training_states:
-        print("Adding training patches from %s" % (state))
-        fn = os.path.join(data_dir, "%s_extended-train_patches.csv" % (state))
-        df = pd.read_csv(fn)
-        for fn in df["patch_fn"].values:
-            training_patches.append((os.path.join(data_dir, fn), state))
-
-    validation_patches = []
-    for state in validation_states:
-        print("Adding validation patches from %s" % (state))
-        fn = os.path.join(data_dir, "%s_extended-val_patches.csv" % (state))
-        df = pd.read_csv(fn)
-        for fn in df["patch_fn"].values:
-            validation_patches.append((os.path.join(data_dir, fn), state))
-
-    print(
-        "Loaded %d training patches and %d validation patches"
-        % (len(training_patches), len(validation_patches))
-    )
-
-    if do_superres:
-        print("Using %d states in superres loss:" % (len(superres_states)))
-        print(superres_states)
-
-    # ------------------------------
-    # Step 2, run experiment
-    # ------------------------------
-    # training_steps_per_epoch = 1#len(training_patches) // batch_size
-    # validation_steps_per_epoch = 1 #len(validation_patches) // batch_size
-
-    training_steps_per_epoch = 300
-    validation_steps_per_epoch = 39
-    # TODO: For testing - best if adaptable
-    if training_steps_per_epoch * batch_size > len(training_patches):
-        training_steps_per_epoch = 1
-        validation_steps_per_epoch = 1
-
-    print(
-        "Number of training/validation steps per epoch: %d/%d"
-        % (training_steps_per_epoch, validation_steps_per_epoch)
-    )
-
-    # Build the model
-    optimizer = RMSprop(learning_rate)
-    if model_type == "unet":
-        model = models.unet((240, 240, 4), 5, optimizer, loss)
-    elif model_type == "unet_large":
-        model = models.unet_large((240, 240, 4), 5, optimizer, loss)
-    elif model_type == "fcdensenet":
-        model = models.fcdensenet((240, 240, 4), 5, optimizer, loss)
-    elif model_type == "fcn_small":
-        model = models.fcn_small((240, 240, 4), 5, optimizer, loss)
-    model.summary()
-
-    validation_callback = utils.LandcoverResults(log_dir=log_dir, verbose=verbose)
-    learning_rate_callback = LearningRateScheduler(
-        utils.schedule_stepped, verbose=verbose
-    )
-
-    model_checkpoint_callback = ModelCheckpoint(
-        os.path.join(log_dir, "model_{epoch:02d}.h5"),
-        verbose=verbose,
-        save_best_only=False,
-        save_weights_only=False,
-        period=20,
-    )
-
-    training_generator = datagen.DataGenerator(
-        training_patches,
-        batch_size,
-        training_steps_per_epoch,
-        240,
-        240,
-        4,
-        do_color_aug=do_color_aug,
-        do_superres=do_superres,
-        superres_only_states=superres_states,
-    )
-    validation_generator = datagen.DataGenerator(
-        validation_patches,
-        batch_size,
-        validation_steps_per_epoch,
-        240,
-        240,
-        4,
-        do_color_aug=do_color_aug,
-        do_superres=do_superres,
-        superres_only_states=[],
-    )
-
-    model.fit_generator(
-        training_generator,
-        steps_per_epoch=training_steps_per_epoch,
-        # epochs=10**6,
-        epochs=num_epochs,
-        verbose=verbose,
-        validation_data=validation_generator,
-        validation_steps=validation_steps_per_epoch,
-        max_queue_size=256,
-        workers=4,
-        use_multiprocessing=True,
-        callbacks=[
-            validation_callback,
-            # learning_rate_callback,
-            model_checkpoint_callback,
-        ],
-        initial_epoch=0,
-    )
-
-    # ------------------------------
-    # Step 3, save models
-    # ------------------------------
-    model.save(os.path.join(log_dir, "final_model.h5"))
-
-    model_json = model.to_json()
-    with open(os.path.join(log_dir, "final_model.json"), "w") as json_file:
-        json_file.write(model_json)
-    model.save_weights(os.path.join(log_dir, "final_model_weights.h5"))
-
-    print("Finished in %0.4f seconds" % (time.time() - start_time))
-    del model, training_generator, validation_generator
+    vars_args = vars(args)
+    args_train = dict((k, vars_args.get(k, None)) for k in ("output",
+    "name",
+    "data_dir",
+    "training_states",
+    "validation_states",
+    "superres_states",))
+    args_experiment = dict((k, vars_args.get(k, None)) for k in ("epochs",
+    "model_type",
+    "batch_size",
+    "learning_rate",
+    "loss",
+    "color"))
+    Train(**args_train).run_experiment(**args_experiment)
 
 
 if __name__ == "__main__":
     main()
-    sys.exit(0)
