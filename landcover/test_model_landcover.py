247ad2c0b5101631228aa226bfb1a16e2ab0b970
Rodrigo Almeida <rodrigo.almeida@up42.com>
2020-02-24 14:58:23 +0100

    Refactor into object

--- landcover/test_model_landcover.py
+++ landcover/test_model_landcover.py
@@ -54,56 +54,6 @@ import keras.models
 import keras.metrics
 
 
-def run_model_on_tile(model, naip_tile, inpt_size, output_size, batch_size):
-    down_weight_padding = 40
-    height = naip_tile.shape[0]
-    width = naip_tile.shape[1]
-
-    stride_x = inpt_size - down_weight_padding * 2
-    stride_y = inpt_size - down_weight_padding * 2
-
-    output = np.zeros((height, width, output_size), dtype=np.float32)
-    counts = np.zeros((height, width), dtype=np.float32) + 0.000000001
-    kernel = np.ones((inpt_size, inpt_size), dtype=np.float32) * 0.1
-    kernel[10:-10, 10:-10] = 1
-    kernel[
-        down_weight_padding : down_weight_padding + stride_y,
-        down_weight_padding : down_weight_padding + stride_x,
-    ] = 5
-
-    batch = []
-    batch_indices = []
-    batch_count = 0
-
-    for y_index in list(range(0, height - inpt_size, stride_y)) + [
-        height - inpt_size,
-    ]:
-        for x_index in list(range(0, width - inpt_size, stride_x)) + [
-            width - inpt_size,
-        ]:
-            naip_im = naip_tile[
-                y_index : y_index + inpt_size, x_index : x_index + inpt_size, :
-            ]
-
-            batch.append(naip_im)
-            batch_indices.append((y_index, x_index))
-            batch_count += 1
-
-    model_output = model.predict(np.array(batch), batch_size=batch_size, verbose=0)
-
-    for i, (y, x) in enumerate(batch_indices):
-        output[y : y + inpt_size, x : x + inpt_size] += (
-            model_output[i] * kernel[..., np.newaxis]
-        )
-        counts[y : y + inpt_size, x : x + inpt_size] += kernel
-
-    return output / counts[..., np.newaxis]
-
-
-# ---------------------------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------------------------
-
-
 def do_args(arg_list, name):
     parser = argparse.ArgumentParser(description=name)
 
@@ -156,115 +106,184 @@ def do_args(arg_list, name):
     return parser.parse_args(arg_list)
 
 
-def main():
-    program_name = "Model inference script"
-    args = do_args(sys.argv[1:], program_name)
-
-    input_fn = args.input_fn
-    data_dir = os.path.dirname(input_fn)
-    output_base = args.output_base
-    model_fn = args.model_fn
-    save_probabilities = args.save_probabilities
-    superres = args.superres
-
-    print("Starting %s at %s" % (program_name, str(datetime.datetime.now())))
-    start_time = float(time.time())
-
-    try:
-        df = pd.read_csv(input_fn)
-        fns = df[["naip-new_fn", "lc_fn", "nlcd_fn"]].values
-    except Exception as e:
-        print("Could not load the input file")
-        print(e)
-        return
-
-    model = keras.models.load_model(
+class Test:
+    def __init__(
+        self,
+        input_fn,
+        output_base,
         model_fn,
-        custom_objects={
-            "jaccard_loss": keras.metrics.mean_squared_error,
-            "loss": keras.metrics.mean_squared_error,
-        },
-    )
+        save_probabilities,
+        superres
+    ):
+        self.input_fn = input_fn
+        self.data_dir = os.path.dirname(input_fn)
+        self.output_base = output_base
+        self.model_fn = model_fn
+        self.save_probabilities = save_probabilities
+        self.superres = superres
+        self.start_time = None
+        self.end_time = None
 
-    if superres:
-        model = keras.models.Model(input=model.inputs, outputs=[model.outputs[0]])
-        model.compile("sgd", "mse")
+    @staticmethod
+    def run_model_on_tile(model, naip_tile, inpt_size, output_size, batch_size):
+        down_weight_padding = 40
+        height = naip_tile.shape[0]
+        width = naip_tile.shape[1]
 
-    output_shape = model.output_shape[1:]
-    input_shape = model.input_shape[1:]
-    model_input_size = input_shape[0]
-    assert (
-        len(model.outputs) == 1
-    ), "The loaded model has multiple outputs. You need to specify --superres if this model was trained with the superres loss."
+        stride_x = inpt_size - down_weight_padding * 2
+        stride_y = inpt_size - down_weight_padding * 2
 
-    for i in range(len(fns)):
-        tic = float(time.time())
-        naip_fn = os.path.join(data_dir, fns[i][0])
-        lc_fn = os.path.join(data_dir, fns[i][1])
-        nlcd_fn = os.path.join(data_dir, fns[i][2])
+        output = np.zeros((height, width, output_size), dtype=np.float32)
+        counts = np.zeros((height, width), dtype=np.float32) + 0.000000001
+        kernel = np.ones((inpt_size, inpt_size), dtype=np.float32) * 0.1
+        kernel[10:-10, 10:-10] = 1
+        kernel[
+            down_weight_padding : down_weight_padding + stride_y,
+            down_weight_padding : down_weight_padding + stride_x,
+        ] = 5
 
-        print("Running model on %s\t%d/%d" % (naip_fn, i + 1, len(fns)))
+        batch = []
+        batch_indices = []
+        batch_count = 0
 
-        naip_fid = rasterio.open(naip_fn, "r")
-        naip_profile = naip_fid.meta.copy()
-        naip_tile = naip_fid.read().astype(np.float32) / 255.0
-        naip_tile = np.rollaxis(naip_tile, 0, 3)
-        naip_fid.close()
+        for y_index in list(range(0, height - inpt_size, stride_y)) + [
+            height - inpt_size,
+        ]:
+            for x_index in list(range(0, width - inpt_size, stride_x)) + [
+                width - inpt_size,
+            ]:
+                naip_im = naip_tile[
+                    y_index : y_index + inpt_size, x_index : x_index + inpt_size, :
+                ]
 
-        output = run_model_on_tile(
-            model, naip_tile, model_input_size, output_shape[2], 16
+                batch.append(naip_im)
+                batch_indices.append((y_index, x_index))
+                batch_count += 1
+
+        model_output = model.predict(np.array(batch), batch_size=batch_size, verbose=0)
+
+        for i, (y, x) in enumerate(batch_indices):
+            output[y : y + inpt_size, x : x + inpt_size] += (
+                model_output[i] * kernel[..., np.newaxis]
+            )
+            counts[y : y + inpt_size, x : x + inpt_size] += kernel
+
+        return output / counts[..., np.newaxis]
+
+    def load_tiles(self):
+        try:
+            df = pd.read_csv(self.input_fn)
+            fns = df[["naip-new_fn", "lc_fn", "nlcd_fn"]].values
+            return fns
+        except Exception as e:
+            print("Could not load the input file")
+            print(e)
+            return
+
+    def load_model(self):
+        model = keras.models.load_model(
+            model_fn,
+            custom_objects={
+                "jaccard_loss": keras.metrics.mean_squared_error,
+                "loss": keras.metrics.mean_squared_error,
+            },
         )
-        # output[:,:,4] += output[:,:,5]
-        # output[:,:,4] += output[:,:,6]
-        output = output[:, :, :5]
 
-        # ----------------------------------------------------------------
-        # Write out each softmax prediction to a separate file
-        # ----------------------------------------------------------------
-        if save_probabilities:
-            output_fn = os.path.basename(naip_fn)[:-4] + "_prob.tif"
+        if superres:
+            model = keras.models.Model(input=model.inputs, outputs=[model.outputs[0]])
+            model.compile("sgd", "mse")
+
+        output_shape = model.output_shape[1:]
+        input_shape = model.input_shape[1:]
+        model_input_size = input_shape[0]
+        assert (
+            len(model.outputs) == 1
+        ), "The loaded model has multiple outputs. You need to specify --superres if this model was trained with the superres loss."
+        return model
+
+    def run_on_tiles(self):
+        print("Starting %s at %s" % (program_name, str(datetime.datetime.now())))
+        self.start_time = float(time.time())
+
+        fns = self.load_tiles()
+        model = self.load_model()
+
+        for i in range(len(fns)):
+            tic = float(time.time())
+            naip_fn = os.path.join(self.data_dir, fns[i][0])
+            lc_fn = os.path.join(self.data_dir, fns[i][1])
+            nlcd_fn = os.path.join(self.data_dir, fns[i][2])
+
+            print("Running model on %s\t%d/%d" % (naip_fn, i + 1, len(fns)))
+
+            naip_fid = rasterio.open(naip_fn, "r")
+            naip_profile = naip_fid.meta.copy()
+            naip_tile = naip_fid.read().astype(np.float32) / 255.0
+            naip_tile = np.rollaxis(naip_tile, 0, 3)
+            naip_fid.close()
+
+            output = run_model_on_tile(
+                model, naip_tile, model.input_shape[1:], model.output_shape[1:][2], 16
+            )
+            # output[:,:,4] += output[:,:,5]
+            # output[:,:,4] += output[:,:,6]
+            output = output[:, :, :5]
+
+            # ----------------------------------------------------------------
+            # Write out each softmax prediction to a separate file
+            # ----------------------------------------------------------------
+            if self.save_probabilities:
+                output_fn = os.path.basename(naip_fn)[:-4] + "_prob.tif"
+                current_profile = naip_profile.copy()
+                current_profile["driver"] = "GTiff"
+                current_profile["dtype"] = "uint8"
+                current_profile["count"] = 5
+                current_profile["compress"] = "lzw"
+
+                # quantize the probabilities
+                bins = np.arange(256)
+                bins = bins / 255.0
+                output = np.digitize(output, bins=bins, right=True).astype(np.uint8)
+
+                f = rasterio.open(
+                    os.path.join(self.output_base, output_fn), "w", **current_profile
+                )
+                f.write(output[:, :, 0], 1)
+                f.write(output[:, :, 1], 2)
+                f.write(output[:, :, 2], 3)
+                f.write(output[:, :, 3], 4)
+                f.write(output[:, :, 4], 5)
+                f.close()
+
+            # ----------------------------------------------------------------
+            # Write out the class predictions
+            # ----------------------------------------------------------------
+            output_classes = np.argmax(output, axis=2).astype(np.uint8)
+            output_class_fn = os.path.basename(naip_fn)[:-4] + "_class.tif"
+
             current_profile = naip_profile.copy()
             current_profile["driver"] = "GTiff"
             current_profile["dtype"] = "uint8"
-            current_profile["count"] = 5
+            current_profile["count"] = 1
             current_profile["compress"] = "lzw"
-
-            # quantize the probabilities
-            bins = np.arange(256)
-            bins = bins / 255.0
-            output = np.digitize(output, bins=bins, right=True).astype(np.uint8)
-
             f = rasterio.open(
-                os.path.join(output_base, output_fn), "w", **current_profile
+                os.path.join(self.output_base, output_class_fn), "w", **current_profile
             )
-            f.write(output[:, :, 0], 1)
-            f.write(output[:, :, 1], 2)
-            f.write(output[:, :, 2], 3)
-            f.write(output[:, :, 3], 4)
-            f.write(output[:, :, 4], 5)
+            f.write(output_classes, 1)
             f.close()
 
-        # ----------------------------------------------------------------
-        # Write out the class predictions
-        # ----------------------------------------------------------------
-        output_classes = np.argmax(output, axis=2).astype(np.uint8)
-        output_class_fn = os.path.basename(naip_fn)[:-4] + "_class.tif"
+            print("Finished iteration in %0.4f seconds" % (time.time() - tic))
 
-        current_profile = naip_profile.copy()
-        current_profile["driver"] = "GTiff"
-        current_profile["dtype"] = "uint8"
-        current_profile["count"] = 1
-        current_profile["compress"] = "lzw"
-        f = rasterio.open(
-            os.path.join(output_base, output_class_fn), "w", **current_profile
-        )
-        f.write(output_classes, 1)
-        f.close()
+        self.end_time = float(time.time())
+        print("Finished %s in %0.4f seconds" % (program_name, self.end_time - self.start_time))
 
-        print("Finished iteration in %0.4f seconds" % (time.time() - tic))
 
-    print("Finished %s in %0.4f seconds" % (program_name, time.time() - start_time))
-
+def main():
+    program_name = "Model inference script"
+    args = do_args(sys.argv[1:], program_name)
+    vars_args = vars(args)
+    print(vars_args)
+    Test(**vars_args).run_on_tiles()
 
 if __name__ == "__main__":
     main()
